fcf2ddf4f2ea4dd8ca9af4af90ba4e52d766210a
Josh Brown <brown.joshua.scott@gmail.com>
2025-01-26 05:20:07 -0600

    Return repeated string for provided number of time

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let returnedString = string;
+    
+    for(let i = 1; i < num; i++) {
+        returnedString = returnedString + string;
+    };
 
+    return returnedString;
 };
 
 // Do not edit below this line
